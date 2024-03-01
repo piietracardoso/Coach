142a416de94a30c306b810b2f645b6b6a08f7f83
piietracardoso <148592386+piietracardoso@users.noreply.github.com>
2024-02-29 21:09:16 -0300

    Atividade 29/02 atualizada
    1) Mensagem "Qntd inválida" na cor vermelha 
    2) Validação do valor unitário
    3) Linha toda vermelha quando valor unitário estiver errado

--- calcula-total.js
+++ calcula-total.js
@@ -1,15 +1,33 @@
 //Captura todos os clientes que fizeram encomendas
 var clientes = document.querySelectorAll(".cliente");
 
-//Passa por cada encomenda, calculando o valor total
-for (var count=0; count < clientes.length; count++){
+
+for (var i=0; i < clientes.length; i++) {
 
     //Captura a quantidade encomendada
-    var qtde = clientes[count].querySelector(".info-qtde").textContent;
-    
-    //Captura o alor unitário do produto
-    var unitário = clientes[count].querySelector(".info-valor").textContent;
+    var qtde = clientes[i].querySelector(".info-qtde").textContent;
 
-    //Calcula o valor total da encomenda 
-    clientes[count].querySelector(".info-total").textContent = qtde * unitário;
+    //Captura o valor untário do produto
+    var unitario = clientes[i].querySelector(".info-valor").textContent;
+
+    //Verifica se a quantidade é um número válido
+    if(qtde < 1 || isNaN(qtde)){
+        clientes[i].querySelector(".info-qtde").textContent = "Qtde inválida";
+        clientes[i].querySelector(".info-qtde").style.color="red";
+    } if (unitario < 1 ) {
+        clientes[i].style.backgroundColor = "red";
+        
+    }else {
+        
+        //A quantidade está certa, pode prosseguir
+        //Exibe o valor total da encomenda 
+        clientes[i].querySelector(".info-total").textContent = calculaTotal(qtde,unitario);
+    }
 }
+
+function calculaTotal (qtde,unitario) {
+    var total = 0;
+    total = qtde * unitario;
+    return total;
+}
+
